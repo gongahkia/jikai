dd463c7495593f3df069905865fa71adfb6a5778
gongahkia <angryapplegravy@gmail.com>
2024-12-23 19:21:03 +0800

    feat(app): fixed corpus tweaking

--- src/main.py
+++ src/main.py
@@ -7,29 +7,21 @@ import helper as h
 if __name__ == "__main__":
 
     TARGET_FILEPATH = "./../corpus/clean/tort/corpus.json"
+    TOPICS = [
+        "negligence",
+        "duty of care",
+        "standard of care",
+        "causation",
+        "remoteness",
+    ]
+    SAMPLE_SIZE = 1
 
     corpus_data = h.load_corpus(TARGET_FILEPATH)
-
     if corpus_data is not None:
-
-        topics = [
-            "negligence",
-            "duty of care",
-            "standard of care",
-            "causation",
-            "remoteness",
-        ]
-
-        relevant_text = h.query_relevant_text(corpus_data, topics)
-
-        # print(f"relevant text identified as: {relevant_text}")
-
+        context = h.query_relevant_text(corpus_data, TOPICS, 1)
+        print(f"relevant text identified as: {context}")
         model = h.start_model()
-
-        print("balls")
-
-        # response = h.query_model(model, vector_store, topics)
-        # print(f"Model Response: {response}")
-
+        response = h.query_model(model, context, TOPICS)
+        print(f"Model Response: {response}")
     else:
         print("Failed to load the corpus.")
